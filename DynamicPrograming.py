f317c78c9f7db2596078d0ef92b19cf6f0986972
Serdar Kuyuk <serdarkuyuk@gmail.com>
2020-11-01 20:40:32 -0500

    complexity added

--- DynamicPrograming.py
+++ DynamicPrograming.py
@@ -1,7 +1,8 @@
 #Dynamic Programming
 
 Longest subsequent subarray
-
+Time complexity O(n2)
+Space complexity O(n)
 ```python
 arr = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
      #[1, 2, 2, 3,  2,  3, 3,  4, 2, 3, 3, 4,  3, 4,  4, 5]
