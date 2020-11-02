58d571319a1e4f70574c18eac2e8423ccd959056
Serdar Kuyuk <serdarkuyuk@gmail.com>
2020-11-01 20:39:22 -0500

    dynamic programming

---
+++ DynamicPrograming.py
@@ -0,0 +1,16 @@
+#Dynamic Programming
+
+Longest subsequent subarray
+
+```python
+arr = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
+     #[1, 2, 2, 3,  2,  3, 3,  4, 2, 3, 3, 4,  3, 4,  4, 5]
+# arr = [-1, 3, 4, 5, 2, 2, 2, 2]
+
+result = [1] * len(arr)
+
+for j in range(1, len(arr)):
+    for i in range(0, j):
+        if arr[j] > arr[i]:
+            result[j]  = max(result[j], result[i]+1)
+```
--- LoopPossibleMoves.md
+++ LoopPossibleMoves.md
@@ -6,4 +6,3 @@ possbileMove = [(1,0),(0,1),(-1,0),(0,-1)]
 for move in possbileMove:
     nexti, nextj = i+move[0], j+move[1]
 ```
-# my
